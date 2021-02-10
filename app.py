c6c390901ed4f81bd809a7682eb4a18fa7cd902e
pkul0001 <54395555+pkul0001@users.noreply.github.com>
2021-02-10 23:33:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import *
 import os
 from werkzeug.utils import secure_filename
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 
